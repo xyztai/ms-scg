f32eba7f4419b7b7ce33ebc1a859328a4c744520
xyztai <taixyz@163.com>
2021-09-13 17:12:07 +0800

    Initial commit

---
+++ gateway/src/main/java/org/m/platform/gateway/SCGApplication.java
@@ -0,0 +1,15 @@
+package org.m.platform.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author tai
+ * @create 2021-09-13 16:52
+ */
+@SpringBootApplication
+public class SCGApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SCGApplication.class, args);
+    }
+}
